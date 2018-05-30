a54aa6f2906812f89b339b217d03c500acdc3b6b
Obada Jabassini <obada.jabassini@gmail.com>
2018-05-30 03:28:58 +0300

    Initial Commit

---
+++ fast_gradient_method.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torchvision import models
+
+from classes import classes
+
+
+class FastGradientMethod:
+
+    def __init__(self, model_name="resnet18"):
+        self.model = getattr(models, model_name)(pretrained=True)
+        self.model.eval().cuda()
+
+    def modify_image(self, image_path, eps=10):
+        orig = cv2.imread(image_path)[..., ::-1]
+        orig = cv2.resize(orig, (224, 224))
+        img = orig.copy().astype(np.float32)
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        img /= 255.0
+        img = (img - mean) / std
+        img = img.transpose(2, 0, 1)
+        inp = Variable(torch.from_numpy(img).cuda().float().unsqueeze(0), requires_grad=True)
+        out = self.model(inp)
+        original_pred = np.argmax(out.data.cpu().numpy())
+        original_class = classes[original_pred].split(',')[0]
+        if eps != 0:
+            adv, perturbation, adv_class = self.modify(img, eps, original_pred, mean, std)
+        else:
+            adv_class = original_class
+            while original_class == adv_class:
+                eps += 1
+                adv, perturbation, adv_class = self.modify(img, eps, original_pred, mean, std)
+        adv_name = "adv.jpg"
+        perturbation_name = "perturbation.png"
+        cv2.imwrite(adv_name, adv)
+        cv2.imwrite(perturbation_name, perturbation)
+        return original_class, adv_class, adv_name, perturbation_name
+
+    def modify(self, img, eps, original_pred, mean, std):
+        criterion = nn.CrossEntropyLoss().cuda()
+        inp = Variable(torch.from_numpy(img).cuda().float().unsqueeze(0), requires_grad=True)
+        out = self.model(inp)
+        loss = criterion(out, Variable(torch.Tensor([float(original_pred)]).cuda().long()))
+        loss.backward()
+        inp.data = inp.data + ((eps / 255.0) * torch.sign(inp.grad.data))
+        inp.grad.data.zero_()
+        adv_pred = np.argmax(self.model(inp).data.cpu().numpy())
+        adv_class = classes[adv_pred].split(',')[0]
+        adv = inp.data.cpu().numpy()[0]
+        perturbation = (adv - img).transpose(1, 2, 0)
+        adv = adv.transpose(1, 2, 0)
+        adv = (adv * std) + mean
+        adv = adv * 255.0
+        adv = adv[..., ::-1]
+        adv = np.clip(adv, 0, 255).astype(np.uint8)
+        return adv, perturbation, adv_class
+
+    def predict_image(self, image_path):
+        orig = cv2.imread(image_path)[..., ::-1]
+        orig = cv2.resize(orig, (224, 224))
+        img = orig.copy().astype(np.float32)
+        mean = [0.485, 0.456, 0.406]
+        std = [0.229, 0.224, 0.225]
+        img /= 255.0
+        img = (img - mean) / std
+        img = img.transpose(2, 0, 1)
+        inp = Variable(torch.from_numpy(img).cuda().float().unsqueeze(0), requires_grad=True)
+        out = self.model(inp)
+        original_pred = np.argmax(out.data.cpu().numpy())
+        return classes[original_pred].split(',')[0]
---
+++ ui.py
@@ -0,0 +1,47 @@
+import sys
+
+from PyQt5 import uic, QtWidgets, QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog
+
+from fast_gradient_method import FastGradientMethod
+
+FormClass = uic.loadUiType("ui.ui")[0]
+
+
+class Ui(QtWidgets.QMainWindow, FormClass):
+    def __init__(self, parent=None):
+        QtWidgets.QMainWindow.__init__(self, parent)
+        self.setupUi(self)
+        self.image_path = None
+        self.model = FastGradientMethod()
+        self.setup_events()
+
+    def setup_events(self):
+        self.loadButton.clicked.connect(self.load_original_image)
+        self.modifyButton.clicked.connect(self.modify)
+
+    def load_original_image(self):
+        image_path, _ = QFileDialog.getOpenFileName(self, 'Choose A File')
+        self.load_image(image_path, self.originalLabel)
+        self.image_path = image_path
+
+    def load_image(self, image_path, label):
+        pixmap = QtGui.QPixmap(image_path)
+        scaled_pixmap = pixmap.scaled(label.size())
+        label.setPixmap(scaled_pixmap)
+
+    def modify(self):
+        eps = self.epsSpinBox.value()
+        original_class, adv_class, adv_path, perturbation_name = self.model.modify_image(self.image_path, eps)
+        self.originalClassLabel.setText(original_class)
+        self.advClassLabel.setText(adv_class)
+        self.load_image(adv_path, self.advLabel)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Ui()
+    ui.setWindowTitle("Seminar Demo")
+    ui.show()
+    app.exec_()
